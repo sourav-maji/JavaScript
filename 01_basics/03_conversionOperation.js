38b9c69f744da1ec90797f1504a0afb125cbbd47
Sourav Maji <s722143@gmail.com>
2023-09-19 10:51:54 +0000

    Postfix and Prefix operator

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -53,10 +53,26 @@ console.log(negValue);
 
 let str1="hello"
 let str2=" hitesh"
-let str3= str1+ str2.  // String concatination
+let str3= str1+ str2; // String concatination
 console.log(str3);
 
 console.log("1"+2); // 12
 console.log(1+ "2"); //12
 console.log("1"+2+2); //122
 console.log(1+2 +"2"); // 32
+
+// Tricky conversion
+console.log(+true);  // this will give output 1 
+// console.log(true+);  // this will give error
+console.log(+"");  // this will give 0\
+
+let num1,num2,num3
+
+num1=num2=num3=2+2;
+console.table([num1,num2,num3])
+console.log(+"10");
+
+// Postfix and Prefix operator
+let gameScore=132;
+gameScore++;
+console.log(gameScore);
