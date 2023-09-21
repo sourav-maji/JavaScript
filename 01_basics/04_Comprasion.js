16be6503d39797070abab94eadd9f7f29feede62
Sourav Maji <s722143@gmail.com>
2023-09-21 05:49:46 +0000

    comprasion

---
+++ 01_basics/04_Comprasion.js
@@ -0,0 +1,15 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+console.log("2">1);
+console.log("02">1);
+console.log(true>2);  // this will give result false
+
+console.table([null>0,null ==0, null>=0])
+console.table([undefined>0, undefined>=0, undefined ==0])
+
+// ===    strict check.This will check data type as well
+
