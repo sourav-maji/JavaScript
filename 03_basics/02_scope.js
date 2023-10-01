65c64997fda87628c01f0ec4be1345ad9b45a3fd
Sourav Maji <s722143@gmail.com>
2023-10-01 09:40:13 +0000

    Scope part 1

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,16 @@
+// var c = 300;
+let a = 300
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 30
+    console.log("Innee : ", a);  // o/p=> 10
+
+}
+
+console.log(a); // o/p => 300
+// console.log(b);
+// console.log(c);  // o/p => 30
+
+// Note : we need to avoid to use var keyword
