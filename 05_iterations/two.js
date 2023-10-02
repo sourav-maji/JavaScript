5302b1ba66b070eadf978be5599250b9a01ab0d8
Sourav Maji <s722143@gmail.com>
2023-10-02 05:22:54 +0000

    while and do-while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+// while loop
+
+let index = 0
+
+while(index <=10){
+    console.log(`Value of index is : ${index}`);
+    index+=2
+}
+
+let myArray = ["batman", "flash" , "superman"]
+let i=0
+while(i < myArray.length){
+    console.log(myArray[i]);
+    i++
+}
+
+// do-while loop
+let score =1
+do{
+    console.log(`Score is : ${score}`);
+    score++
+}while(score<=10)
