95ca538f760bab1a550911aacb7fc37abfeab2f2
Sourav Maji <s722143@gmail.com>
2023-10-04 15:17:45 +0000

    for of loop in string

--- 05_iterations/three.js
+++ 05_iterations/three.js
@@ -7,5 +7,15 @@
 // }
 const arr = [1,2,3,4,5]
 for ( const num of arr){
-    console.log(num);
+    // console.log(num);
 }
+
+const greetings = "hello world!!"
+for(const greet of greetings){
+    if(greet==" "){
+        continue;
+    }
+    console.log(`Each char is : ${greet}`);
+}
+
+// Maps
