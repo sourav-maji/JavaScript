239228d646af26519d336449113b91cbd2b388fe
Sourav Maji <s722143@gmail.com>
2023-10-07 15:47:14 +0000

    forEach in Array of Objects

--- 05_iterations/five.js
+++ 05_iterations/five.js
@@ -19,5 +19,24 @@ function printMe(item) {
 coding.forEach ( printMe )  // We have to give the reference of the function ; we don't have to execute the function here
 
 coding.forEach((item , index , arr) =>{
-    console.log(item , index , arr);
+    // console.log(item , index , arr);
 })
+
+const myCoding = [
+    {
+        languageName : "javascript",
+        languageFileName : "js"
+    },
+    {
+        languageName : "java",
+        languageFileName : "java"
+    },
+    {
+        languageName : "python",
+        languageFileName : "py"
+    }
+]
+
+myCoding.forEach ( (item)=>{
+    console.log(item.languageName);
+} )
