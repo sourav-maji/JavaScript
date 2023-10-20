53ed68c13b775df142f6962d0662485192ebb0e8
Sourav Maji <s722143@gmail.com>
2023-10-20 03:09:15 +0000

    2nd part

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -22,3 +22,37 @@ new Promise(function (resolve , reject){
 }).then( function (){
     console.log("Promise Consumed 2");
 })
+
+
+const promiseThree = new Promise(function (resolve , reject){
+        setTimeout(function ( ){
+                resolve({userName : "Chai", email : "chai@chaiaurcode.com"})
+        } ,1000)
+})
+
+promiseThree.then( function (user ) {
+        console.log(user);
+})
+
+const promiseFour =  new Promise( function (resolve, reject) {
+    setTimeout( function(){
+        let err = false    
+        //  true
+        if (!err){
+            resolve({userName:"Sourav", password :"1234"})
+        }else{
+                reject("ERROR : Something went wrong")
+        }
+    }, 1000)    
+})
+
+
+promiseFour.then( (user)=>{
+    console.log(user);
+    return user.username
+}).then( (username)=>{
+        console.log(username);
+}).catch(function(error){
+    console.log(error);
+})
+
