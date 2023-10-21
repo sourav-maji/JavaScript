87191e1654c5c5a940870a9c1a00ecd653e10b94
Sourav Maji <s722143@gmail.com>
2023-10-21 01:10:27 +0000

    Promise Five

--- 09_advance_one/promise.js
+++ 09_advance_one/promise.js
@@ -83,9 +83,24 @@ async function consumePromiseFive(){
 async function getAllUsers(){
     try{
         const response = await fetch("https://jsonplaceholder.typicode.com/users")
-        const data = response.json()
+        const data = await response.json()
         console.log(data);
     } catch(error){
-        console.log(error);
+        console.log("ER : ",error);
     }
 }
+
+// getAllUsers()
+
+fetch("https://jsonplaceholder.typicode.com/users")
+.then( (response)=>{
+    return response.json()
+})
+.then((data)=>{
+    console.log(data);
+})
+.catch( (error)=>{
+    console.log(error);
+})
+
+
