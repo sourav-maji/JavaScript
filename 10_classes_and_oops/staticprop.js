ba829c58cadc233925f89c2bf14bac1f9af86f82
Sourav Maji <s722143@gmail.com>
2023-10-21 16:20:07 +0000

    static in js

---
+++ 10_classes_and_oops/staticprop.js
@@ -0,0 +1,31 @@
+class User {
+    constructor (username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username : ${this.username}`);
+    }
+
+    static createId(){
+        return `123`
+    }
+
+}
+
+const hitesh = new User ("hitesh")
+hitesh.logMe()
+// console.log(hitesh.createId())
+
+class Teacher extends User {
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+iphone.logMe();
+// console.log(iphone.createId());
+
